d98f847652de40b23f2563cb4f0aab9f3cdb477a
David Barnes <david.barnes@heretik.com>
2017-10-02 17:54:47 -0500

    Cleanup

--- nuget/build.ps1
+++ nuget/build.ps1
@@ -7,14 +7,17 @@ Push-Location $PSScriptRoot
 
 #copy Dll
 "Copying ..\ApplicationParser\bin\Release\ApplicationParser.dll => .\lib\net45"
+
+New-Item -ItemType File -Path .\lib\net45\ApplicationParser.dll -Force | out-null
 Copy-Item ..\ApplicationParser\bin\Release\ApplicationParser.dll .\lib\net45
 
-Remove-Item ".\content\CodeGen\*"
+Remove-Item ".\content\CodeGen\*" -ErrorAction SilentlyContinue | out-null
 Get-ChildItem ..\gen\*.pp | % {
     $destFile = ".\content\CodeGen\$($_.Name)";
 
     Write-Output "Replacing tokens $($_.Name) => $($destFile)"
-    (Get-Content $_.FullName).Replace("{{nuget-version}}", $Version) | Set-Content $destFile
+	New-Item -ItemType File -Path $destFile -Force | out-null
+    (Get-Content $_.FullName).Replace("{{nuget-version}}", $Version) | Set-Content $destFile | out-null
 }
 
 nuget pack -Version $Version
