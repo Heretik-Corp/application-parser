ca956e3cd48b95e7dd0e12b9a023ebeaf3fa1970
David Barnes <dvbarnes89@gmail.com>
2017-11-30 15:25:02 -0600

    broke up the code to support outside consumption

--- nuget/build.ps1
+++ nuget/build.ps1
@@ -5,13 +5,26 @@ param(
 )
 Push-Location $PSScriptRoot
 
+Remove-Item ".\content\" -Force -Recurse -ErrorAction SilentlyContinue | out-null
+Remove-Item .\lib -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+
+
+"Generating Core Package"
 #copy Dll
 "Copying ..\ApplicationParser\bin\Release\ApplicationParser.dll => .\lib\net45"
 
 New-Item -ItemType File -Path .\lib\net45\ApplicationParser.dll -Force | out-null
 Copy-Item ..\ApplicationParser\bin\Release\ApplicationParser.dll .\lib\net45
 
-Remove-Item ".\content\CodeGen\*" -ErrorAction SilentlyContinue | out-null
+
+nuget pack .\ApplicationParser.Core.nuspec -Version $Version
+
+"Generating T4 Package"
+
+Remove-Item .\lib -Force -Recurse | Out-Null
+New-Item -ItemType File -Path .\lib\net45\ApplicationParser.Generator.T4.dll -Force | out-null
+Copy-Item ..\ApplicationParser.Generator.T4\bin\Release\ApplicationParser.Generator.T4.dll .\lib\net45
+
 Get-ChildItem ..\gen\*.pp | % {
     $destFile = ".\content\CodeGen\$($_.Name)";
 
@@ -20,5 +33,12 @@ Get-ChildItem ..\gen\*.pp | % {
     (Get-Content $_.FullName).Replace("{{nuget-version}}", $Version) | Set-Content $destFile | out-null
 }
 
-nuget pack -Version $Version
+(Get-Content .\ApplicationParser.nuspec.gen).Replace("{{version}}", $version) | Set-Content .\ApplicationParser.nuspec | out-null
+
+nuget pack .\ApplicationParser.nuspec -Version $Version
+
+Remove-Item .\ApplicationParser.nuspec
+Remove-Item ".\content\" -Force -Recurse -ErrorAction SilentlyContinue | out-null
+Remove-Item .\lib -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+
 Pop-Location
